b774d84956ab1279d35811a7b1401734aaaed83f
Jan Flokstra <jan.flokstra@utwente.nl>
2017-05-17 15:40:08 +0200

    Add Javadoc for main TemplateController class

--- src/main/java/hmi/flipper2/TemplateController.java
+++ src/main/java/hmi/flipper2/TemplateController.java
@@ -8,8 +8,33 @@ import java.util.List;
 import hmi.flipper2.postgres.Database;
 import hmi.flipper2.sax.SimpleSAXParser;
 
+/**
+ * This class is the main Flipper interface class. You can create/modify/destroy Flipper Templatecontollers using this class. 
+ * An example of how to use this class can be found in the Main class 
+ * 
+ * @author Jan Flokstra
+ * @version 1.0
+ * 
+ */
+
 public class TemplateController {
 		
+	/**
+	 * This method creates a new TemplateController.
+	 * 
+	 * @param name
+	 *            The name of the TemplateController. When db != null this name
+	 *            has to be unique .
+	 * @param descr
+	 *            A description what this Controller does and is used for.
+	 * @param db
+	 *            The database used by this TemplateController, may be null for
+	 *            non-persistent Controller.
+	 * @return A running TemplateController. This TemplateController is
+	 *         persistent in the database when db != null
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public static TemplateController create(String name, String descr, Database db) throws FlipperException {
 		if ( db != null ) {
 				db.createController(name, descr);
@@ -18,6 +43,16 @@ public class TemplateController {
 		return new TemplateController(name, db);
 	}
 	
+	/**
+	 * This method destroys a persistent TemplateController.
+	 * 
+	 * @param name
+	 *            The name of the existing TemplateController in the Database.
+	 * @param db
+	 *            The Database the TemplateController is stored. When db == null the method does nothing.
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public static void destroy(String name, Database db) throws FlipperException {
 		if ( db != null ) {
 				db.destroyController(name);
@@ -35,6 +70,19 @@ public class TemplateController {
 	private List<TemplateFile> tf_list;
 	public  Is is;
 	
+	/**
+	 * This method constructs a running TemplateController.
+	 * 
+	 * @param name
+	 *            The name of the TemplateController. When db != null the
+	 *            TemplateController name has to exist in the Database.
+	 *            Otherwise use TemplateController.create().
+	 * @param db
+	 *            The database used by this TemplateController, may be null for
+	 *            a non-persistent Controller.
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public TemplateController(String name, Database db) throws FlipperException {
 		this.name = name;
 		this.db = db;
@@ -48,6 +96,16 @@ public class TemplateController {
 		}
 	}
 	
+	/**
+	 * This method adds an Xml template file to a running TemplateController.
+	 * When the TemplateController is persistent in a Database this template
+	 * file is persistently added to the controller.
+	 * 
+	 * @param path
+	 *            The path of the XML template file
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public void addTemplateFile(String path) throws FlipperException {
 		try {
 			String xml_str = SimpleSAXParser.readFile(path);
@@ -60,6 +118,17 @@ public class TemplateController {
 		}
 	}
 	
+	/**
+	 * This method checks all Templates if the preconditions are true and fires
+	 * the Effects and Behaviours when necessary. When the controller is
+	 * persistent the Information State is saved in the Datababase and the
+	 * Database state is committed.
+	 * 
+	 * @return a boolean value indicating that some Template preconditions were
+	 *         true.
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public boolean checkTemplates() throws FlipperException {
 		boolean changed = false;
 
@@ -74,20 +143,45 @@ public class TemplateController {
 		return changed;
 	}
 	
+	/**
+	 * This method returns the String JSON value of the Information State path variable.
+	 * 
+	 * @param path The Information state variable path as used by Javascript
+	 * @return The String JSON Value of the JavaScript Is variable.
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
 	public String getIs(String path) throws FlipperException {
 		return is.getIs(path);
 	}
 	
-	public void close() {
+	/**
+	 * This method closes a TemplateController. In case of a non-persistent
+	 * controller all data will be lost otherwise the controller is persistent
+	 * in the Database and can be reopened in the future.
+	 * 
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
+	public void close() throws FlipperException {
 		this.name = null;
 		this.db = null;
 		this.is = null;
 	}
 	
-	public String resourcePath(String r) throws FlipperException {
-		URL url = this.getClass().getClassLoader().getResource(r);
+	/**
+	 * This method returns the absolute filename of a project resource on the host filesystem.
+	 * 
+	 * @param rpath
+	 *            The relative Path of the resource in the project tree.
+	 * @return The absolute path of the file in the host filesystem.
+	 * @exception FlipperException
+	 *                On all errors.
+	 */
+	public String resourcePath(String rpath) throws FlipperException {
+		URL url = this.getClass().getClassLoader().getResource(rpath);
 		if ( url == null )
-			throw new FlipperException("Resource file: " + r + " not found");
+			throw new FlipperException("Resource file: " + rpath + " not found");
 		return url.getPath();
 	}
 	
